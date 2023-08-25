ee849d5dc722fcca828c85575705c6fc340f8a51
Yoseph Berhane <yosephbet@gmail.com>
2023-08-25 08:39:40 +0200

    Add code for quote

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // index.js
 // const apiKey = API_KEY;
 
-// ...
+// Fetching quote of the day from an API
+fetch('https://api.quotable.io/random')
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('quote').innerHTML = `${data.content} - ${data.author}`;
+  })
+  .catch(error => console.error(error));
